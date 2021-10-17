db8ffb17f7bb33813a02f3330051999422a96a2d
Jeff Barriault <jeff@barriault.net>
2021-10-17 11:51:16 -0500

    changes

--- app/concepts/application/cell/nav_item.rb
+++ app/concepts/application/cell/nav_item.rb
@@ -1,5 +1,16 @@
 module Application::Cell
   class NavItem < Trailblazer::Cell
+
+    private
+
+    def active?
+      return 'nav-item active' if nav_label.eql?(controller.class.name.split('::').first)
+
+      return 'nav-item active' if "#{nav_label}Controller".eql?(controller.class.name.split('::').first)
+
+      'nav-item'
+    end
+
     def nav_label
       model.model_name.human.pluralize
     end
